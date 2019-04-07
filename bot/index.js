5b8f56c88c3b0dc23bc6d19eb8cfa261680af83d
Bfas237 <gwatanajohn3@gmail.com>
2019-04-07 03:07:53 +0100

    ...

--- bot/index.js
+++ bot/index.js
@@ -5,23 +5,25 @@ const config = require('../config');
 const bot = new Telegraf(process.env.TOKEN);
 
 const http = require('http');
+const token = process.env.TOKEN;
 
+const Bot = require('node-telegram-bot-api');
+let bot;
 
 if(process.env.NODE_ENV === 'production') {
-  bot.telegram.setWebhook(process.env.HEROKU_URL + process.env.TOKEN);
+  bot.launch({
+  webhook: {
+    domain: process.env.HEROKU_URL + process.env.TOKEN,
+    port: process.env.PORT
+  }
+})
+
 }
 else {
   bot.startPolling();
 }
 
-
-//app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(process.env.HEROKU_URL + process.env.TOKEN);
-}, 280000);
-
 console.log('Your app is listening on port ' + 3000 + ' \u{1F604}');
-
 
 
 module.exports = bot;
---
+++ web.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const packageInfo = require('./package.json');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, "0.0.0.0", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+module.exports = (bot) => {
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/' + bot.token, (req, res) => res.render('pages/index'))
+};

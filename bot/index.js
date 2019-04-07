334e8bd2c0ada99ac326c837ce432fc90d49130d
Glitch (elfin-afterthought) <none>
2019-04-07 00:26:48 +0000

    💊👚 Updated with Glitch

--- bot/index.js
+++ bot/index.js
@@ -3,24 +3,26 @@
 const Telegraf = require('telegraf');
 const config = require('../config');
 const bot = new Telegraf(process.env.TOKEN);
-bot.telegram.setWebhook(`https://${process.env.PROJECT_DOMAIN}.herokuapp.com/webhook`);
+
 const http = require('http');
-const express = require('express');
-const app = express();
-app.get("http://${process.env.PROJECT_DOMAIN}.herokuapp.com/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
+
+
+if(process.env.NODE_ENV === 'production') {
+  bot.telegram.setWebhook(process.env.HEROKU_URL + process.env.TOKEN);
+}
+else {
+  bot.startPolling();
+}
+
+
 //app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.herokuapp.com/`);
+  http.get(process.env.HEROKU_URL + process.env.TOKEN);
 }, 280000);
 
 console.log('Your app is listening on port ' + 3000 + ' \u{1F604}');
 
-if (process.env.NODE_ENV === 'development') {
-	bot.polling.offset = -1;
-}
+
 
 module.exports = bot;
 // cyclic dependency
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 
 process.chdir(__dirname);
-
+const express = require('express');
+const app = express();
 // Utils
 const { logError } = require('./utils/log');
-const express = require('express')
 /**
  * @type {Telegraf}
  * Bot
  */
+require("dotenv").config();
 const bot = require('./bot');
 var nodemon = require('nodemon');
 
@@ -17,6 +18,10 @@ nodemon({
   ext: 'js json'
 });
 
+app.get(process.env.HEROKU_URL + process.env.TOKEN, (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
 nodemon.on('start', function () {
   console.log('App has started');
 }).on('quit', function () {
@@ -25,11 +30,35 @@ nodemon.on('start', function () {
 }).on('restart', function (files) {
   console.log('App restarted due to: ', files);
 });
+
+const bodyParser = require('body-parser');
+const packageInfo = require('./package.json');
+
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: packageInfo.version });
+});
+
+var server = app.listen(process.env.PORT, "0.0.0.0", () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+module.exports = (bot) => {
+  app.post('/' + bot.token, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+};
+
 bot.telegram.getMe().then((botInfo) => {
 	bot.options.username = botInfo.username;
 	bot.context.botInfo = botInfo;
 }).then(() => {
-  bot.startWebhook('/webhook', null, '3000');
+  //bot.startWebhook('/webhook', null, '3000');
 	//bot.startPolling();
 });
 
